43de4690221dbee495e268d7388052413e91b529
lakhara <siddharth.lakhara@gmail.com>
2018-01-31 11:34:20 +0530

    Prob.10 -> returns a rejected promise

--- tenthExercise/solution.test.js
+++ tenthExercise/solution.test.js
@@ -4,11 +4,10 @@ test('Receives promise: ', (done)=>{
 	expect(soln).toBeInstanceOf(Promise);
 	done();
 });
-/*
+
 test('verifying error message: ', (done)=>{
 	soln.catch((err)=>{
 		expect(err.message).toBe('OH NOES');
 	});
 	done();
 });
-*/
